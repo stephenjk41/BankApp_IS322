dcf26f4a8c630e1caf1e43dc95c3bfc12cb31949
Karm Desai <karmdesai@Karms-MacBook-Pro.local>
2020-05-15 12:28:47 -0400

    Edit

--- banking-app/src/components/App.js
+++ banking-app/src/components/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 import AccountList from './AccountList';
 
-// const View = () => {
-//     return (
-//         <div>
-//         <Media queries={{
-//           phone: "(max-width: 411px)",
-//           small_desktop: "(min-width: 768px) and (max-width: 1024px)",
-//           large_desktop: "(min-width: 1366px)",
-//           tablet: "(max-width: 768)"
-//         }}>
-//           {matches => (
-//             <Fragment>
-//               {matches.Phone && <p>I am Phone</p>}
-//               {matches.small_desktop && <p>I am medium!</p>}
-//               {matches.large_desktop && <p>I am large!</p>}
-//               {matches.tablet && <p>I am Tablet</p>}
-//             </Fragment>
-//           )}
-//         </Media>
-//       </div>
-//     )
-// }
+const View = () => {
+    return (
+        <div>
+        <Media queries={{
+          phone: "(max-width: 411px)",
+          small_desktop: "(min-width: 768px) and (max-width: 1024px)",
+          large_desktop: "(min-width: 1366px)",
+          tablet: "(max-width: 768)"
+        }}>
+            </Media>
+      </div>
+    )
+}
 
 const App = () => {
     return (
